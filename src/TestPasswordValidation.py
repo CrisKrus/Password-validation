e371f9827203730b1ae881fd567609f4eb16a0a5
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 18:05:41 +0000

    Green: test__given_password_with_less_than_eight_chars

--- src/TestPasswordValidation.py
+++ src/TestPasswordValidation.py
@@ -1,6 +1,10 @@
 import unittest
 
 
+def validatePassword(password):
+    return False
+
+
 class TestPasswordValidation(unittest.TestCase):
 
     def test__given_password_with_less_than_eight_chars(self):
