40679a7ef23c5b3d8f6a129442b5ea015f54a96d
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 18:09:15 +0000

    Red: test__given_password_with_less_than_eight_chars

--- src/TestPasswordValidation.py
+++ src/TestPasswordValidation.py
@@ -8,4 +8,5 @@ def validatePassword(password):
 class TestPasswordValidation(unittest.TestCase):
 
     def test__given_password_with_less_than_eight_chars(self):
-        self.assertEqual(False, validatePassword(['q', 'w', 'e', 'r', 't']))
+        self.assertFalse(validatePassword(['q', 'w', 'e', 'r', 't']))
+        self.assertTrue(validatePassword(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i']))
