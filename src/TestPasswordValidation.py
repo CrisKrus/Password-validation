1d1c55804a23ef56bb97e794047aef8bc40fcb8d
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 19:03:15 +0000

    Green: test__given_password_with_at_least_one_upper_case_char

--- src/TestPasswordValidation.py
+++ src/TestPasswordValidation.py
@@ -1,15 +1,20 @@
 import unittest
 
 
-def validate_password(password):
-    if len(password) < 8:
+def validate_password(password, length):
+    if length < 8:
         return False
+    if password[0].isupper():
+        return True
+    else:
+        return validate_password(password[1:length], length)
 
+    return False
 
 class TestPasswordValidation(unittest.TestCase):
 
     def test__given_password_with_less_than_eight_chars(self):
-        self.assertFalse(validate_password(['q', 'w', 'e', 'r', 't']))
+        self.assertFalse(validate_password(['q', 'w', 'e', 'r', 't'], 5))
 
     def test__given_password_with_at_least_one_upper_case_char(self):
-        self.assertTrue(validate_password(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'W']))
+        self.assertTrue(validate_password(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'W'], 11))
