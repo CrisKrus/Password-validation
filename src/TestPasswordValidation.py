1f5871bc96aaadd945e9d10fdef69dd0d0fe8e8c
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 18:56:08 +0000

    Refactor: delete one assert from test because it not correct

--- src/TestPasswordValidation.py
+++ src/TestPasswordValidation.py
@@ -1,12 +1,13 @@
 import unittest
 
 
-def validatePassword(password):
-    return True if len(password) > 7 else False
+def validate_password(password):
+    if len(password) < 8:
+        return False
 
 
 class TestPasswordValidation(unittest.TestCase):
 
     def test__given_password_with_less_than_eight_chars(self):
-        self.assertFalse(validatePassword(['q', 'w', 'e', 'r', 't']))
-        self.assertTrue(validatePassword(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i']))
+        self.assertFalse(validate_password(['q', 'w', 'e', 'r', 't']))
+
