69aad81e9ebcf7451dbd32ba4f8bda62e7570354
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 18:59:04 +0000

    Red: test__given_password_with_at_least_one_upper_case_char

--- src/TestPasswordValidation.py
+++ src/TestPasswordValidation.py
@@ -11,3 +11,5 @@ class TestPasswordValidation(unittest.TestCase):
     def test__given_password_with_less_than_eight_chars(self):
         self.assertFalse(validate_password(['q', 'w', 'e', 'r', 't']))
 
+    def test__given_password_with_at_least_one_upper_case_char(self):
+        self.assertTrue(validate_password(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'W']))
